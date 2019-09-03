263881ff12bd1148ed7612ac389221b151296837
Vinita Gupta <vinita.gupta@publicissapient.com>
2019-09-03 10:34:21 +0530

    make it better

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'src/root-application/root-application.js'));
 });
 
 app.listen(process.env.PORT || 8000, function(){
